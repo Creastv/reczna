29c05e1a34ce5b36a76ffc37cc4a901d22c7bdec
Creastv <creastv@gail.com>
2025-10-24 09:14:02 +0200

    gtm

--- header.php
+++ header.php
@@ -2,6 +2,28 @@
 <html <?php language_attributes(); ?>>
 
 <head>
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-W8PQ9TWZ');
+    </script>
+    <!-- End Google Tag Manager -->
+
+<body>:
+    <!-- Google Tag Manager (noscript) -->
+
     <meta name="theme-color" content="#fff">
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -12,16 +34,20 @@
     <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Oswald:wght@200..700&display=swap"
         rel="stylesheet">
     <?php wp_head(); ?>
-</head>
+    </head>
 
-<body <?php body_class(); ?>>
-    <header id="header" class="js-header" itemscope itemtype="http://schema.org/WPHeader">
-        <?php get_template_part('templates-parts/header/header', 'brand'); ?>
-        <?php get_template_part('templates-parts/header/header', 'nav'); ?>
-        <?php get_template_part('templates-parts/header/header', 'burger'); ?>
-        <?php get_template_part('templates-parts/header/header', 'partners'); ?>
-    </header>
+    <body <?php body_class(); ?>>
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W8PQ9TWZ" height="0" width="0"
+                style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
+        <header id="header" class="js-header" itemscope itemtype="http://schema.org/WPHeader">
+            <?php get_template_part('templates-parts/header/header', 'brand'); ?>
+            <?php get_template_part('templates-parts/header/header', 'nav'); ?>
+            <?php get_template_part('templates-parts/header/header', 'burger'); ?>
+            <?php get_template_part('templates-parts/header/header', 'partners'); ?>
+        </header>
 
 
-    <main id="main">
-        <div class="container">
+        <main id="main">
+            <div class="container">
